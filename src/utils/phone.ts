5ac5743f64ae4d28be234b1f0119c5c1f0595a4f
minhminh03 <haiminh17052003@gmail.com>
2025-09-17 18:39:09 +0700

    push

--- src/utils/phone.ts
+++ src/utils/phone.ts
@@ -1,18 +1,25 @@
-export const PHONE_NUMBER = "0929884950";
-export const PHONE_NUMBER_WITH_COUNTRY_CODE = "+84929884950";
+const DOMAIN_PHONE_MAP: Record<string, string> = {
+    "slmsolar.com": "0964920242",
+    "slmsolar.com.vn": "0977879291",
+    "slmsolar.vn": "0929884950",
+};
+
+export function getPhoneNumberByDomain(domain?: string): string {
+    if (!domain && typeof window !== "undefined") {
+        domain = window.location.hostname.replace(/^www\./, "");
+    }
+    return DOMAIN_PHONE_MAP[domain || "slmsolar.vn"] || "0929884950";
+}
+
+export const PHONE_NUMBER = getPhoneNumberByDomain();
+export const PHONE_NUMBER_WITH_COUNTRY_CODE = "+84" + PHONE_NUMBER.slice(1);
 
 export const formatPhoneNumber = (phone: string): string => {
-    // Remove all non-digit characters
     const cleaned = phone.replace(/\D/g, '');
-    
-    // Format as xxx.xxx.xxx
     return cleaned.replace(/(\d{3})(\d{3})(\d{4})/, '$1.$2.$3');
 };
 
 export const formatPhoneNumberWithCountryCode = (phone: string): string => {
-    // Remove all non-digit characters
     const cleaned = phone.replace(/\D/g, '');
-    
-    // Format as +84 (xx) xxx-xxxx
     return cleaned.replace(/(\d{2})(\d{3})(\d{4})/, '+84 ($1) $2-$3');
-}; 
+};
